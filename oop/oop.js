50c46e7366dcc3f3506fc7dd626dd818816bcba4
williamChann <841131780@qq.com>
2017-10-18 15:37:03 +0800

    add oop

---
+++ oop/oop.js
@@ -0,0 +1,21 @@
+//构造函数
+var Student = {
+    name: 'Robot',
+    height: 1.2,
+    run: function() {
+        console.log(this.name + ' is running...');
+    }
+};
+
+function createStudent(name) {
+    // 基于Student原型创建一个新对象:Object.create()
+    var s = Object.create(Student);
+    // 初始化新对象:
+    s.name = name;
+    return s;
+}
+
+var xiaoming = createStudent('小明');
+console.log(xiaoming); //{name: '小明'}
+xiaoming.run(); // 小明 is running...
+xiaoming.__proto__ === Student; // true
