114db4a7697e2fa1e87b4087cdd508894c40159e
williamChann <841131780@qq.com>
2018-02-24 18:44:10 +0800

    modify

--- 10-js拓展/2-yuanyifeng/deferred.js
+++ 10-js拓展/2-yuanyifeng/deferred.js
@@ -2,11 +2,36 @@
  * Deferred对象
  */
 var dtd = $.Deferred();     //新建一个deferred对象
+var wait = function (dtd) {
+    var task = function () {
+        alert('执行完毕');
+        dtd.resolve();  //改变执行状态为已完成
+        //dtd.reject(); //改变执行状态为已失败
+    }
+    setTimeout(task, 5000);
+    return dtd;
+}
 
-var wait = function (dtd) {  
+/**
+ * $.when()参数为deferred（延迟）对象
+ */
+$.when(wait(dtd))
+.done(function () { alert("哈哈哈，成功了") })
+.fail(function () { alert("哈哈哈，失败了") });
+dtd.resolve();
+
+
+var wait = function () {
+    var dtd = $.Deferred();     //新建一个deferred对象
     var  task = function(){
         alert('执行完毕');
         dtd.resolve();
     }
-    
+    setTimeout(task, 5000);
+
+    return dtd.promise();   //没有参数时，返回一个新的deferred对象，该对象的运行状态无法被改变
 }
+
+$.when(wait())
+.done(function () { alert("哈哈哈，成功了") }) //弹出了“执行完毕”再弹
+.fail(function () { alert("哈哈哈，失败了") });
